1fb4dccceb16ad35e34a54fffced5f4f2a8f48fe
linqh <linqh@yidoutang.com>
2021-08-27 22:51:27 +0800

    feat: add 237

---
+++ src/easy/_237DeleteNodeInALinkedList.kt
@@ -0,0 +1,15 @@
+package easy
+
+import ListNode
+
+class _237DeleteNodeInALinkedList {
+class Solution {
+    fun deleteNode(node: ListNode?) {
+        if (node == null) {
+            return
+        }
+        node.`val` = node.next!!.`val`
+        node.next = node.next?.next
+    }
+}
+}
