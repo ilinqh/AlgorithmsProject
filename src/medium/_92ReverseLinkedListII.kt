a959e6602588edc8ae29dabe731a693ecb161a42
iLinQH <649995267@qq.com>
2021-09-10 15:07:01 +0800

    feat: complete 92

--- src/medium/_92ReverseLinkedListII.kt
+++ src/medium/_92ReverseLinkedListII.kt
@@ -8,8 +8,26 @@ class _92ReverseLinkedListII {
             if (head?.next == null || left == right) {
                 return head
             }
+            val preHead = ListNode(0)
+            preHead.next = head
+            var preV = preHead
+            var tempLeft = left
+            while (tempLeft > 1) {
+                preV = preV.next!!
+                tempLeft -= 1
+            }
+            val curr = preV.next
+            var next: ListNode?
+            var index = 0
+            while (index < right - left) {
+                next = curr!!.next!!
+                curr.next = next.next
+                next.next = preV.next
+                preV.next = next
+                index += 1
+            }
 
-            return null
+            return preHead.next
         }
     }
 }
