f07aeba29216badf9a98d64b65f450e68cf2cebc
iLinQH <649995267@qq.com>
2021-09-18 09:42:56 +0800

    feat: complete today record

--- src/easy/_292NimGame.kt
+++ src/easy/_292NimGame.kt
@@ -3,7 +3,8 @@ package easy
 class _292NimGame {
     class Solution {
         fun canWinNim(n: Int): Boolean {
-            return (n % 4) != 0
+            // (n and 3) != 0 等价于 n % 4 != 0
+            return n % 4 != 0
         }
     }
 }
