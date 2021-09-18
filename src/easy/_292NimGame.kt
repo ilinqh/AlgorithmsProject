0985e1cbc329ccd8b5d61852f2a19bc2d08d3dc4
iLinQH <649995267@qq.com>
2021-09-18 09:52:53 +0800

    doc: add doc

--- src/easy/_292NimGame.kt
+++ src/easy/_292NimGame.kt
@@ -3,6 +3,8 @@ package easy
 class _292NimGame {
     class Solution {
         fun canWinNim(n: Int): Boolean {
+            // a % b 时，如果 b 为 2 的 n 次方，则可通过位运算替换取模预算
+            // a % b = a & (b - 1)[b = 2 的 n 次方] 即：a % (2 的 n 次方) = a & ((2 的 n 次方) - 1)
             // (n and 3) != 0 等价于 n % 4 != 0
             return n % 4 != 0
         }
