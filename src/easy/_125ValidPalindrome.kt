00b30aad3390496194c391e48e7f27fb50d13eac
iLinQH <649995267@qq.com>
2021-09-23 16:56:39 +0800

    feat: complete 125

---
+++ src/easy/_125ValidPalindrome.kt
@@ -0,0 +1,26 @@
+package easy
+
+class _125ValidPalindrome {
+    class Solution {
+        fun isPalindrome(s: String): Boolean {
+            var left = 0
+            var right = s.length - 1
+            while (left < right) {
+                while (left < right && !s[left].isLetterOrDigit()) {
+                    left += 1
+                }
+                while (left < right && !s[right].isLetterOrDigit()) {
+                    right -= 1
+                }
+                if (left < right) {
+                    if (s[left].toLowerCase() != s[right].toLowerCase()) {
+                        return false
+                    }
+                    left += 1
+                    right -= 1
+                }
+            }
+            return true
+        }
+    }
+}
