e414757f6451a9c8ec11be74e0e88677f44e4640
iLinQH <649995267@qq.com>
2021-10-21 23:03:14 +0800

    feat: complete 25

--- src/hard/_25ReverseNodesInKGroup.kt
+++ src/hard/_25ReverseNodesInKGroup.kt
@@ -3,45 +3,46 @@ package hard
 import ListNode
 
 class _25ReverseNodesInKGroup {
-    class Solution {
-        fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
-            if (head?.next == null || k == 1) {
-                return head
-            }
-            val preHead = ListNode(0)
-//            preHead.next = head
-            var pre: ListNode? = null
-            var cur = head
-            var step: Int
-            var first = true
-            while (cur != null) {
-                step = k
-                var tail = cur
-                var reverse = false
-                while (tail != null && step > 0) {
-                    tail = tail.next
-                    step -= 1
-                    reverse = step == 0
-                }
-                if (reverse) {
-                    step = k
-                    var next: ListNode?
-                    while (step > 0) {
-                        next = cur?.next
-                        cur?.next = pre
-                        pre = cur
-                        cur = next
-                        step -= 1
-                    }
-                    if (first) {
-                        first = false
-                        preHead.next = pre
-                    }
-                } else {
-                    break
-                }
-            }
-            return preHead.next
+class Solution {
+    fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        if (head?.next == null || k == 1) {
+            return head
         }
+        var tempHead: ListNode? = head
+        val preHead = ListNode(0)
+        preHead.next = tempHead
+        var pre: ListNode? = preHead
+
+        while (tempHead != null) {
+            var tail = pre
+            for (i in 0 until k) {
+                tail = tail?.next
+                if (tail == null) {
+                    return preHead.next
+                }
+            }
+            val next: ListNode? = tail!!.next
+            val reverse = myReserve(tempHead, tail)
+            tempHead = reverse[0]
+            tail = reverse[1]
+            pre?.next = tempHead
+            tail.next = next
+            pre = tail
+            tempHead = tail.next
+        }
+        return preHead.next
     }
+
+    private fun myReserve(head: ListNode, tail: ListNode): Array<ListNode> {
+        var preV = tail.next
+        var p: ListNode? = head
+        while (preV != tail) {
+            val next = p?.next
+            p?.next = preV
+            preV = p
+            p = next
+        }
+        return arrayOf(tail, head)
+    }
+}
 }
