b1b22e36322f3ccf6b56eb137f8315ccb3672b03
iLinQH <649995267@qq.com>
2021-12-31 10:57:45 +0800

    feat: complete today record

---
+++ src/easy/_507PerfectNumber.kt
@@ -0,0 +1,9 @@
+package easy
+
+class _507PerfectNumber {
+    class Solution {
+        fun checkPerfectNumber(num: Int): Boolean {
+            return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336
+        }
+    }
+}
