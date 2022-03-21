60ba2453dec93c18918d9c06816f28b63e24ee47
iLinQH <649995267@qq.com>
2022-03-21 22:54:45 +0800

    feat: complete today record

--- src/easy/_653TwoSumIvInputIsABst.kt
+++ src/easy/_653TwoSumIvInputIsABst.kt
@@ -3,28 +3,28 @@ package easy
 import TreeNode
 
 class _653TwoSumIvInputIsABst {
-class Solution {
-    fun findTarget(root: TreeNode?, k: Int): Boolean {
-        if (root == null) {
-            return false
+    class Solution {
+        fun findTarget(root: TreeNode?, k: Int): Boolean {
+            if (root == null) {
+                return false
+            }
+            val map = HashMap<Int, Int>()
+            return bfs(root, k, map)
         }
-        val map = HashMap<Int, Int>()
-        return bfs(root, k, map)
-    }
 
-    private fun bfs(root: TreeNode?, k: Int, map: HashMap<Int, Int>): Boolean {
-        if (root == null) {
-            return false
+        private fun bfs(root: TreeNode?, k: Int, map: HashMap<Int, Int>): Boolean {
+            if (root == null) {
+                return false
+            }
+            if (map.containsKey(root.`val`)) {
+                return true
+            }
+            map[k - root.`val`] = root.`val`
+            val leftResult = bfs(root.left, k, map)
+            if (leftResult) {
+                return true
+            }
+            return bfs(root.right, k, map)
         }
-        if (map.containsKey(root.`val`)) {
-            return true
-        }
-        map[k - root.`val`] = root.`val`
-        val leftResult = bfs(root.left, k, map)
-        if (leftResult) {
-            return true
-        }
-        return bfs(root.right, k, map)
     }
-}
 }
