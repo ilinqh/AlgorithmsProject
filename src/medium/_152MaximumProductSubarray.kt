6a577e1f4c8c9d1f6ce757e8f698aecf2f126cbe
iLinQH <649995267@qq.com>
2022-08-01 12:02:23 +0800

    feat: complete 152

--- src/medium/_152MaximumProductSubarray.kt
+++ src/medium/_152MaximumProductSubarray.kt
@@ -3,7 +3,18 @@ package medium
 class _152MaximumProductSubarray {
     class Solution {
         fun maxProduct(nums: IntArray): Int {
-            return 0
+            val length = nums.size
+            var ans = nums[0]
+            var maxF = nums[0]
+            var minF = nums[0]
+            for (i in 1 until length) {
+                val tempMax = maxF
+                val tempMin = minF
+                maxF = Math.max(tempMax * nums[i], Math.max(tempMin * nums[i], nums[i]))
+                minF = Math.min(tempMin * nums[i], Math.min(tempMax * nums[i], nums[i]))
+                ans = Math.max(ans, maxF)
+            }
+            return ans
         }
     }
 }
